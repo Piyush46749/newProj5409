a152ec5394c3a64a8290df3b62f3126ce7a858f2
piyush <piyush46749@gmail.com>
2020-02-28 03:57:37 -0400

    fibonacci calculation

---
+++ input.py
@@ -0,0 +1,12 @@
+import random
+import string
+import time
+from datetime import datetime
+
+file=open("input.txt", "w")
+
+each =1
+while each<101:
+	random_number=random.randint(8,20)
+	file.write(str(random_number)+"\n")
+	each+=1
